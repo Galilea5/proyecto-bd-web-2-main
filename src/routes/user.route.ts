f0f89c4e68208443f08207720c698f0404b3e037
Galilea5 <tuemail@tudominio.com>
2024-03-01 23:15:55 -0600

    Avance de proyecto 2

--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,17 +1,27 @@
 import express from 'express'
 import { User } from '../types/user.type'
 import UserService from '../services/user.service'
-import boom from '@hapi/boom'
 
 const router = express.Router()
 const service = new UserService()
 
 router.post('/', async (req, res, next) => {
   try {
-    //TODO: Validate user data coming from the request
     const user: User = req.body
     const newUser = await service.create(user)
-    res.status(201).json({ user: newUser })
+    res.status(201).json({ user: newUser.toClient() })
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const { email } = req.query
+    const user = await service.findByEmail(email as string)
+    console.log({ user })
+
+    res.status(200).json({ user: user.toClient() })
   } catch (error) {
     next(error)
   }
